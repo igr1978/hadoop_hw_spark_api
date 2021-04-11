474659d02192eddac26156f9e2da7254e3e82dbc
igr1978 <i.gavako@gmail.com>
2021-04-11 16:04:46 +0300

    Spark Api

--- build.sbt
+++ build.sbt
@@ -7,6 +7,8 @@ scalaVersion := "2.12.13"
 // https://mvnrepository.com/artifact/org.apache.spark/spark-sql
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.0.2"
 
-
 // https://mvnrepository.com/artifact/org.postgresql/postgresql
 libraryDependencies += "org.postgresql" % "postgresql" % "42.2.19"
+
+// https://mvnrepository.com/artifact/joda-time/joda-time
+libraryDependencies += "joda-time" % "joda-time" % "2.10.10"
--- src/main/scala/HomeWorkTaxiRDD.scala
+++ src/main/scala/HomeWorkTaxiRDD.scala
@@ -2,6 +2,9 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, Dataset, SaveMode, SparkSession}
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 object HomeWorkTaxiRDD extends App {
   Logger.getLogger("org").setLevel(Level.OFF)
 
@@ -27,15 +30,21 @@ object HomeWorkTaxiRDD extends App {
   val taxiFactsDS: Dataset[TaxiRide] = taxiFactsDF.as[TaxiRide]
   val taxiFactsRDD: RDD[TaxiRide] = taxiFactsDS.rdd
 
-  val taxiTimeRDD: RDD[(String, Int)] = taxiFactsRDD
-    .map(x => (x.tpep_pickup_datetime.substring(11, 16), 1))
+  val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+  val hh_mm = DateTimeFormatter.ofPattern("HH:mm")
+
+  val taxiTimeRDD = taxiFactsRDD
+    //.map(x => (x.tpep_pickup_datetime.substring(11, 16), 1))
+    .map(x => (LocalDateTime.parse(x.tpep_pickup_datetime, formatter).format(hh_mm), 1))
     .reduceByKey(_ + _)
     .sortBy(_._2, false)
+    .map(z => z._1 + " " + z._2)
+    .coalesce(1, false)
 
   taxiTimeRDD.take(20).foreach(x => println(x))
 
   val header: RDD[String] = spark.sparkContext.parallelize(Array("pickup_time count"))
-  header.union(taxiTimeRDD.map(z => z._1 + " " + z._2))
+  header.union(taxiTimeRDD)
     .coalesce(1 , true)
     //.saveAsTextFile(time_orders)
     .toDF().write.mode(SaveMode.Overwrite).text(time_orders)
